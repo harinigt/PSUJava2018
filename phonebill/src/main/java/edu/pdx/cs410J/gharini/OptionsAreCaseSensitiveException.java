e9ec8bbee6b965d223ea120c793b02f9ccb7f67c
user1133 <harinitulasidas@gmail.com>
2018-07-10 12:16:50 -0700

    Committing all exception classes and latest project1 class

---
+++ phonebill/src/main/java/edu/pdx/cs410J/gharini/OptionsAreCaseSensitiveException.java
@@ -0,0 +1,15 @@
+package edu.pdx.cs410J.gharini;
+
+
+
+public class OptionsAreCaseSensitiveException extends RuntimeException {
+
+    public  OptionsAreCaseSensitiveException(){
+
+    }
+
+    public OptionsAreCaseSensitiveException( String msg){
+        super(msg);
+
+    }
+}
